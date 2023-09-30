975531910a75efec52fed117333491738e376a1c
krawtschenko <eugenykravchenko@gmail.com>
2023-09-30 13:08:14 +0200

    counter-v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,60 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import {Wrapper, WrapperButtons, WrapperDisplay} from "./style/Wrapper";
 import {Button} from "./style/Button";
+import {Input} from "./style/Input";
 
 function App() {
-	const [count, setCount] = useState<number>(0)
-	const min = 0, max = 5
+	const [input, setInput] = useState({min: 0, max: 5})
+	const [count, setCount] = useState<number>(input.min)
+	const [text, setText] = useState<string | null>(null)
+
+	function increment() {
+		setCount(prevState => prevState + 1)
+	}
+
+	function reset() {
+		setCount(input.min)
+	}
+
+	function changeInputValueMin(value: ChangeEvent<HTMLInputElement>) {
+		setInput({min: Number(value.currentTarget.value), max: input.max})
+		setText('Click Set')
+	}
+
+	function changeInputValueMax(value: ChangeEvent<HTMLInputElement>) {
+		setInput({min: input.min, max: Number(value.currentTarget.value)})
+		setText('Click Set')
+	}
+
+	function setValue() {
+		setCount(input.min)
+		setText(null)
+	}
+
+	const verificationMin = (input.min >= input.max) || (input.min < 0)
+	const verificationMax = input.max < 0
 
 	return (
 		<div className="App">
 			<Wrapper>
 				<WrapperDisplay>
-					<input type="text"/>
-					<input type="text"/>
+					<Input onChange={(event) => changeInputValueMin(event)} value={input.min} verification={verificationMin}/>
+					<Input onChange={(event) => changeInputValueMax(event)} value={input.max} verification={verificationMax}/>
 				</WrapperDisplay>
 				<WrapperButtons>
-					<Button>Inc</Button>
+					<Button disabled={verificationMin || verificationMax} onClick={setValue}>Set</Button>
 				</WrapperButtons>
 			</Wrapper>
 			<Wrapper>
-				<WrapperDisplay current={count} max={max}>
-					<h3>{count}</h3>
+				<WrapperDisplay current={count} max={input.max}>
+					<h3>
+						{text || count}
+					</h3>
 				</WrapperDisplay>
 				<WrapperButtons>
-					<Button disabled={count === max} onClick={() => setCount(count => count + 1)}>Inc</Button>
-					<Button disabled={count === min} onClick={() => setCount(min)}>Reset</Button>
+					<Button disabled={(count === input.max) || (text !== null)} onClick={increment}>Inc</Button>
+					<Button disabled={(count === input.min) || (text !== null)} onClick={reset}>Reset</Button>
 				</WrapperButtons>
 			</Wrapper>
 		</div>
---
+++ src/style/Input.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Input = styled.input.attrs({
+	type: "number"
+})<IInput>`
+  padding: 10px;
+  width: 70%;
+  border: 2px rgb(8 126 164) solid;
+  border-radius: 10px;
+  color: rgb(8 126 164);
+  font-size: 20px;
+  background: ${({verification}) => verification ? 'darkred' : 'none'};
+`;
+
+interface IInput {
+	verification: boolean
+}
